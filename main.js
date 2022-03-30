739d54dfef3cdf44f13bf9c1a66d62fd2d28cc62
Thinkgamingboy <97239364+Thinkgamingboy@users.noreply.github.com>
2022-03-30 22:52:48 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function but2(){
+    window.open('https://www.britannica.com/place/Canada');
+}
+
+function but3(){
+    window.open('https://en.wikipedia.org/wiki/Canada');
+}
+
+function but4(){
+    print()
+}
